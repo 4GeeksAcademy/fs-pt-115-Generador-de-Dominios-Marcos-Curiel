9ca93a17ad91bb0a517e9c1b04faaa9d1303a76b
MarcosCuriel <Marcoscuriel7@hotmail.com>
2025-06-23 13:15:22 +0000

    estructura hecha

--- src/app.js
+++ src/app.js
@@ -5,7 +5,15 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//Variables Main
+let pronoun = ['the', 'our', 'your'];
+let adj = ['great', 'big', 'small'];
+let noun = ['jogger', 'racoon', 'panda'];
+let domains = [".com", ".info", "net", ".org", ".biz", ".edu"];
+
+const domain = pronoun.flatMap(pronouns => adj.flatMap(adjs => noun.flatMap(nouns => domains.map(domain => `${pronouns}${adjs}${nouns}${domain}`))));
+
+console.log(domain);
+
+
+
